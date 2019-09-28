5adc464313a5cc9863ac94b192d6c99131f763db
Ben Pomerenke <bpomerenke@gmail.com>
2019-09-28 06:13:19 -0500

    basic mqtt (need certs) - BP

--- ios/dummy_iot/dummy_iot/ViewController.swift
+++ ios/dummy_iot/dummy_iot/ViewController.swift
@@ -7,14 +7,92 @@
 //
 
 import UIKit
+import CocoaMQTT
 
 class ViewController: UIViewController {
-
+    let defaultHost = "ab5bhz2ubggz4-ats.iot.us-east-2.amazonaws.com"
+    var mqtt: CocoaMQTT?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sendMessage(_ sender: Any) {
+        print("sending...")
+        let message = "Hello there"
+        
+        mqtt!.publish("bp-test/messages", withString: message, qos: .qos1)
+    }
+    @IBAction func connect(_ sender: Any) {
+        print("connecting...")
+        let clientID = "CocoaMQTT-sample-" + String(ProcessInfo().processIdentifier)
+        mqtt = CocoaMQTT(clientID: clientID, host: defaultHost, port: 1883)
+        mqtt!.username = ""
+        mqtt!.password = ""
+        mqtt!.willMessage = CocoaMQTTWill(topic: "/will", message: "dieout")
+        mqtt!.keepAlive = 60
+        mqtt!.delegate = self
+        mqtt!.enableSSL = true
+    }
+    
+}
 
+// From: https://github.com/emqx/CocoaMQTT
+
+extension ViewController: CocoaMQTTDelegate {
+    // Optional ssl CocoaMQTTDelegate
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        print("trust: \(trust)")
+        /// Validate the server certificate
+        ///
+        /// Some custom validation...
+        ///
+        /// if validatePassed {
+        ///     completionHandler(true)
+        /// } else {
+        ///     completionHandler(false)
+        /// }
+        completionHandler(true)
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print("ack: \(ack)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didStateChangeTo state: CocoaMQTTConnState) {
+        print("new state: \(state)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("publish message: \(message.string!.description), id: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        print("id: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        print("recv'd message: \(message.string!.description), id: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topics: [String]) {
+        print("topics: \(topics)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("topic: \(topic)")
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        print()
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        print()
+    }
+
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        print("err: \(err.debugDescription)")
+    }
 }
 
