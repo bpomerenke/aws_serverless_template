454685920257b3dbf6cebe5fa7bf0d0123f83e41
Ben Pomerenke <bpomerenke@gmail.com>
2019-09-28 05:25:22 -0500

    initial app - BP

---
+++ ios/dummy_iot/dummy_iot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dummy_iot
+//
+//  Created by Benjamin Pomerenke on 9/28/19.
+//  Copyright © 2019 Benjamin Pomerenke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
