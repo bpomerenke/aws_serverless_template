25defd72b98e70d8ba2b0b8bc004e3adb4303887
Ben Pomerenke <bpomerenke@gmail.com>
2019-10-14 22:54:43 -0500

    adding appConfig to deployment - BP

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ cd infrastructure
 terraform apply -auto-approve
 
 cd -
-aws s3 sync web/dist/web/ s3://${TF_VAR_domain_name} --delete
+aws s3 sync web/dist/web/ s3://${TF_VAR_domain_name} --delete --exclude "*/appConfig.json"
+
+cd -
+REST_API_ID=$(aws apigateway get-rest-apis | jq ".items[]|select(.name | contains(\"Data API\")) | .id" -r)
+aws apigateway create-deployment --rest-api-id "${REST_API_ID}" --stage-name="deployed"
--- infrastructure/web.tf
+++ infrastructure/web.tf
@@ -18,6 +18,22 @@ data "template_file" "web_s3_policy" {
     bucket_name = "${var.domain_name}"
   }
 }
+data "template_file" "web_appConfig" {
+  template = "${file("web-appConfig.tpl.json")}"
+
+  vars = {
+    apiBaseUrl   = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/deployed"
+  }
+}
+
+resource "aws_s3_bucket_object" "appConfig" {
+  bucket       = "${aws_s3_bucket.web_s3.id}"
+  key          = "assets/data/appConfig.json"
+  content      = "${data.template_file.web_appConfig.rendered}"
+  etag         = "$${data.template_file.web_appConfig.rendered.md5}"
+  content_type = "application/json"
+}
+
 
 output "s3_web_url" {
     value = "${aws_s3_bucket.web_s3.website_endpoint}"
