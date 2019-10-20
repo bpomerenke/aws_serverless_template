e7bbd74a043e5fc21d0b7ac67e870edf48198ff0
Ben Pomerenke <bpomerenke@gmail.com>
2019-10-19 23:28:34 -0500

    initial version rendering - BP

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private version:string;
+
+  ngOnInit(){
+    this.version = '0.1';
+  }
 }
