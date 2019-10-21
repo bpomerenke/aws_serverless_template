38ec8d886394b4af5a50351eff1c494f6af9c857
Ben Pomerenke <bpomerenke@gmail.com>
2019-10-21 13:26:18 -0500

    extracting commong lambda wiring - BP

--- infrastructure/api-lambda/main.tf
+++ infrastructure/api-lambda/main.tf
@@ -1,3 +1,4 @@
+# Lambda
 module "lambda" {
   source        = "../lambda"
   source_bucket = "${var.source_bucket}"
@@ -16,6 +17,7 @@ resource "aws_lambda_permission" "permission" {
   source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/*/*"
 }
 
+# API Gateway
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${var.rest_api_id}"
   resource_id             = "${var.resource_id}"
@@ -24,6 +26,87 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.lambda_arn}/invocations"
   integration_http_method = "POST"
   passthrough_behavior    = "WHEN_NO_TEMPLATES"
+}
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  authorization = "NONE"
+  http_method   = "${var.http_method}"
+
+  request_parameters = {
+    "method.request.header.ContentType" = true
+    "method.request.header.Accept"      = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "response" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "${aws_api_gateway_method.method.http_method}"
+  status_code   = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+# OPTIONS (CORS)
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "${aws_api_gateway_method.options_method.http_method}"
+  status_code   = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  depends_on = ["aws_api_gateway_method.options_method"]
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "${aws_api_gateway_method.options_method.http_method}"
+  type          = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+   "statusCode": 200
+}
+EOF
+  }
+
+  depends_on = ["aws_api_gateway_method.options_method"]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${var.resource_id}"
+  http_method   = "${aws_api_gateway_method.options_method.http_method}"
+  status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,DELETE'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = ["aws_api_gateway_method_response.options_200"]
 }
 
 
