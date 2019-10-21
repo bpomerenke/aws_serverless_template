02c7889decb67fb0ac7a56a9af1a09f51bd4fce9
Ben Pomerenke <bpomerenke@gmail.com>
2019-10-20 21:45:27 -0500

    getting e2e working - BP

--- web/e2e/src/app.e2e-spec.ts
+++ web/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to web!');
+    expect(page.getTitleText()).toEqual('Welcome to AWS Serverless Web Example');
   });
 
   afterEach(async () => {
